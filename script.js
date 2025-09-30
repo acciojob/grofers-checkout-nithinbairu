c176ef78e5510355f192c20928b12d7b53e31f56
nithinbairu <113778598+nithinbairu@users.noreply.github.com>
2025-09-30 23:57:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,19 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	let sum=0
+  const price=document.querySelectorAll(".price");
+	for(let i=0;i<price.length;i++){
+		sum+=parseInt(price[i].innerText,10)
+	}
+	const table=document.querySelector("table");
+	const row=document.createElement("tr");
+	row.innerHTML=`
+	<td>${sum}</td>
+	`;
+	table.appendChild(row);
 };
+
 
 getSumBtn.addEventListener("click", getSum);
 
